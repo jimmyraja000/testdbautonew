4d7b6f5fdbbf157dfd518b313eb9ef2e70ec9ed0
jimmy <jimmyraja000@gmail.com>
2022-10-09 16:18:41 +0530

    1st

---
+++ testdbautonew/Person/Tables/employee.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [person].[employee] (
+    [Id]       INT        NOT NULL,
+    [emp_id]   NCHAR (10) NULL,
+    [emp_name] CHAR (10)  NULL,
+    [salary]   INT        NULL,
+    [address]  NCHAR (10) NULL,
+    [phone]    NCHAR (10) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
