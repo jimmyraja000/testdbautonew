3a9e56b171ace60c336df4a41fcfb44c7dba5bf0
jimmy <jimmyraja000@gmail.com>
2022-10-09 15:40:09 +0530

    1st

---
+++ testdb/dbo/Tables/STATION.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[STATION] (
+    [ID]     INT       NOT NULL,
+    [CITY]   CHAR (20) NULL,
+    [STATE]  CHAR (2)  NULL,
+    [LAT_N]  REAL      NULL,
+    [LONG_W] REAL      NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
