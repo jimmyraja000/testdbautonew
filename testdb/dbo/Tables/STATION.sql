b710a16690602dc321afe7d20acbd2c0a82cd9d9
jimmyraja000 <92245249+jimmyraja000@users.noreply.github.com>
2022-10-09 16:20:11 +0530

    Delete testdb directory

--- testdb/dbo/Tables/STATION.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[STATION] (
-    [ID]     INT       NOT NULL,
-    [CITY]   CHAR (20) NULL,
-    [STATE]  CHAR (2)  NULL,
-    [LAT_N]  REAL      NULL,
-    [LONG_W] REAL      NULL,
-    PRIMARY KEY CLUSTERED ([ID] ASC)
-);
-
